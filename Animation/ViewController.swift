ea67a57e067da913e6613346265927314c893c6d
Mykola Omelianov <mykola_omelianov@epam.com>
2019-03-21 17:50:08 +0200

    multiplier added

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+typealias tupleVar = (CGRect,CGRect,CGRect)
 class ViewController: UIViewController {
 
     private var leftLabel = UILabel()
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     private var startButton = UIButton()
     private var centerL = CGPoint()
     private var centerR = CGPoint()
+    private var centerB = CGPoint()
     private var readyToAnimate = true
     private let distance = 500
     lazy var screenWidth: CGFloat = { [unowned self] in
@@ -23,28 +24,19 @@ class ViewController: UIViewController {
     private lazy var screenHeight = { [unowned self] in
         return self.view.frame.height
     }()
+    private lazy var multiplier = {[unowned self] in
+        // compare to Xr 896.0
+        return (self.view.frame.height/896.0 )
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let leftX = Int(screenWidth)/4
-        let rightX = Int(screenWidth) * 3 / 4
-        let initialY = Int(screenHeight) / 6
-        let btnPoint = CGPoint(x: 0, y: 0)
-        let pointL = CGPoint(x: leftX-50, y: initialY)
-        let pointR = CGPoint(x: rightX-100, y: initialY)
-        let size = CGSize(width: 150, height: 30)
-        let bRect = CGRect(origin: btnPoint, size: size)
-        let lRect = CGRect(origin: pointL, size: size)
-        let rRect = CGRect(origin: pointR, size: size)
-        
-        leftLabel = setLabel(frame: lRect, bgColor: .red, text: "Spring")
-        centerL = leftLabel.center
-        rightLabel = setLabel(frame: rRect, bgColor: .green, text: "Move")
-        centerR = rightLabel.center
-        startButton = setButton(frame: bRect, bgColor: UIColor.yellow , text: "Start")
+        let sizes = setSizes()
+        leftLabel = setLabel(frame: sizes.0, bgColor: .red, text: "Spring")
+        rightLabel = setLabel(frame: sizes.1, bgColor: .green, text: "Move")
+        startButton = setButton(frame: sizes.2, bgColor: UIColor.yellow , text: "Start")
+        updateSavedPositions()
     }
-
     private func setLabel(frame: CGRect,bgColor: UIColor, text : String) -> UILabel {
         let view = UILabel(frame: frame)
         view.text = text
@@ -53,34 +45,31 @@ class ViewController: UIViewController {
         self.view.addSubview(view)
         return view
     }
-    private func setButton(frame: CGRect,bgColor: UIColor, text : String) -> UIButton {
+    private func setButton(frame: CGRect, bgColor: UIColor, text : String) -> UIButton {
+
         let button = UIButton(frame: frame)
         button.setTitle(text, for: .normal)
         button.tintColor = .black
         button.setTitleColor(UIColor.black, for: .normal)
         button.backgroundColor = bgColor
         button.center.x = self.view.center.x
-        button.center.y = screenHeight - 200
+        button.center.y = screenHeight - 200*multiplier
         button.addTarget(self, action: #selector(animateTapped), for: .touchUpInside)
         self.view.addSubview(button)
         return button
     }
     
     @objc private func animateTapped() {
+
         if readyToAnimate {
-            onStartAnimation(view: self.leftLabel, distance: CGFloat(self.distance), completion: backWardAnimation() )
-            onStartAnimation(view: self.rightLabel, distance: -1000, completion: {}())
+            onStartAnimation(view: self.leftLabel, distance: CGFloat(self.distance)*multiplier, completion: backWardAnimation() )
+            onStartAnimation(view: self.rightLabel, distance: -1000*multiplier, completion: {}())
         } else {
-            leftLabel.center = centerL
-            rightLabel.center = centerR
+            stopAnimation()
         }
-        if readyToAnimate {
-            self.startButton.setTitle("Reset", for: .normal)
-            readyToAnimate = false
-        } else {
-            self.startButton.setTitle("Start", for: .normal)
-            readyToAnimate = true
-        }
+        let txt = readyToAnimate ? "Reset": "Start"
+        self.startButton.setTitle(txt, for: .normal)
+        readyToAnimate = readyToAnimate ? false : true
     }
    private func backWardAnimation() {
         UIView.animate(withDuration: 4, delay: 0,
@@ -99,6 +88,29 @@ class ViewController: UIViewController {
                        completion: { (finish : Bool) in
                         completion }
         )
+    }
+    func stopAnimation() {
+        self.leftLabel.center = self.centerL
+        self.rightLabel.center = self.centerR
+    }
+    func updateSavedPositions(){
+        centerL = leftLabel.center
+        centerR = rightLabel.center
+        centerB = startButton.center
+    }
+    func setSizes() -> tupleVar {
+        let leftX = screenWidth/4
+        let rightX = screenWidth * 3 / 4
+        let initialY = screenHeight / 6
+        let btnPoint = CGPoint(x: 0, y: 0)
+        let pointL = CGPoint(x: leftX-(50.0 * multiplier), y: initialY)
+        let pointR = CGPoint(x: rightX-(100.0 * multiplier), y: initialY)
+        let size = CGSize(width: 150*multiplier, height: 30*multiplier)
+        let bRect = CGRect(origin: btnPoint, size: size)
+        let lRect = CGRect(origin: pointL, size: size)
+        let rRect = CGRect(origin: pointR, size: size)
+        
+        return (lLabel: lRect, rLabel: rRect, btn: bRect)
     }
 }
 
